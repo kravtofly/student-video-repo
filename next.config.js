91101b61a1e2999ae4b93b1b291520cda567f004
Krāv <chris@kravtofly.com>
2025-09-10 07:42:22 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   async headers() {
     return [
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },      // TEMP: open, we’ll tighten later
+          { key: 'Access-Control-Allow-Origin', value: '*' },   // TEMP to unblock
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
           { key: 'Access-Control-Allow-Headers', value: '*' },
           { key: 'Access-Control-Max-Age', value: '86400' },
