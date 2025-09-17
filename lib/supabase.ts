66bd09b9af5153276e1b0f03f9a45cb6e8a496e4
Krāv <chris@kravtofly.com>
2025-09-17 08:30:13 -0500

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!, // must be the SERVICE ROLE
+  // IMPORTANT: service role key, not the anon key
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
   { auth: { persistSession: false } }
 );
