a355157499c20fd6424827cc187382edba076774
Krāv <chris@kravtofly.com>
2025-09-18 23:12:16 -0500

    Update ClientMuxPlayer.tsx

--- app/coach/ClientMuxPlayer.tsx
+++ app/coach/ClientMuxPlayer.tsx
@@ -1,17 +1,24 @@
 "use client";
 import Script from "next/script";
 
-export default function ClientMuxPlayer({ playbackId, token }: { playbackId: string; token: string }) {
+export default function ClientMuxPlayer({
+  playbackId,
+  token,
+}: { playbackId: string; token: string }) {
   return (
     <>
-      <Script src="https://unpkg.com/@mux/mux-player" strategy="afterInteractive" />
+      <Script
+        type="module"
+        src="https://unpkg.com/@mux/mux-player@latest"
+        strategy="afterInteractive"
+      />
       <mux-player
         stream-type="on-demand"
         playback-id={playbackId}
         playback-token={token}
         playsinline
-        style={{ width: "100%", aspectRatio: "16/9", borderRadius: 12 }}
         controls
+        style={{ width: "100%", aspectRatio: "16/9", borderRadius: 12 }}
       />
     </>
   );
