2f4c796a48cfdb216b0c054b6fb3b15261bcec57
Krāv <chris@kravtofly.com>
2025-10-06 09:38:53 -0500

    Update notes.ts

--- pages/api/svr/notes.ts
+++ pages/api/svr/notes.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { supabaseServer } from '../../../lib/supabase'
-import { withCORS } from '../../../lib/cors'
-
+import { supabaseAdmin } from '../../../lib/supabase';
+import { withCORS } from '../../../lib/cors';
 
 export default withCORS(async function handler(req: NextApiRequest, res: NextApiResponse) {
 if (req.method === 'GET') {
