dcfb4c271f32f9c032ab440401734231f91deaf4
Krāv <chris@kravtofly.com>
2025-10-06 09:39:41 -0500

    Update mark-reviewed.ts

--- pages/api/svr/mark-reviewed.ts
+++ pages/api/svr/mark-reviewed.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { withCORS } from '../../../lib/cors'
+import { supabaseAdmin } from '../../../lib/supabase';
+import { withCORS } from '../../../lib/cors';
 
 
 export default withCORS(async function handler(req: NextApiRequest, res: NextApiResponse) {
